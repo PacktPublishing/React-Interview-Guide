5079ab2003de71f47bfd142989dfe3865ee1ed57
Sudheer Jonna <jsudheer50@gmail.com>
2023-10-13 16:50:38 +0800

    Chapter11 format fixes

--- Chapter11/one-stop-electronics/src/app/store/root-reducer.ts
+++ Chapter11/one-stop-electronics/src/app/store/root-reducer.ts
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux"
-
 import { userReducer } from "./user/user.slice"
 import { productsReducer } from "./product/product.slice"
 import { cartReducer } from "./cart/cart.slice"
